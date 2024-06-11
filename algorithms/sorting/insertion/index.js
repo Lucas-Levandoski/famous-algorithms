44026d7465a1f27fef0ab308d9c1ce4a307550fa
Lucas Levandoski <lucas.levandoski@bp.com>
2024-06-11 17:54:28 -0300

    fixing insertion sort (it was iterating through too much)

--- algorithms/sorting/insertion/index.js
+++ algorithms/sorting/insertion/index.js
@@ -7,8 +7,10 @@ function InsertionSort(arr) {
 
   for(let i = 1; i < arr.length; i++) {
     for(let j = i; j >= 0; j--) {
-      if(arr[j] < arr[j-1]) [arr[j], arr[j-1]] = [arr[j-1], arr[j]];
-      
+      if(arr[j] < arr[j-1]) 
+        [arr[j], arr[j-1]] = [arr[j-1], arr[j]];
+      else break;
+
       executions ++;
     }
   }
